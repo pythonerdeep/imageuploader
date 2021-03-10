314e2500d5376985bb0fdf4da941ee519ec58012
pythonerdeep <deepugupta930@gamil.com>
2021-03-10 11:39:19 +0530

    intialize first

---
+++ myapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Image
+# Register your models here.
+@admin.register(Image)
+class ImageField(admin.ModelAdmin):
+    list_display=['id','photo','date']
